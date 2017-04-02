269dce9abb54cfe6095b9c5c541a9a89580930df
zaneta989 <zaneta989@gmail.com>
2017-04-02 17:53:21 +0200

    zmiana aplikacji na kalkurator BMR

--- app/src/main/java/com/example/zaneta/lab2_cz2/OknoGlowne.java
+++ app/src/main/java/com/example/zaneta/lab2_cz2/OknoGlowne.java
@@ -12,12 +12,6 @@ public class OknoGlowne extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_okno_glowne);
     }
-    public void wyswietlanieOknaSzL(View view) {
-        final Intent intencja2 = new Intent(this,WyswietlanieDodanegoProduktu.class); startActivity(intencja2);
-    }
-    public void wyswietlanieOknaL(View view) {
-        final Intent intencja2 = new Intent(this,WyswietlanieListy.class); startActivity(intencja2);
-    }
     public void wyswietlanieOknaD(View view) {
         final Intent intencja2 = new Intent(this,kolejne.class); startActivity(intencja2);
     }
